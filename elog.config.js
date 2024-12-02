e1d21da1e51f3cc7c330d1eb4b42ba26cf852397
EmccK <wangkai9799@gmail.com>
2024-12-02 22:55:29 +0800

    修改config.js

--- elog.config.js
+++ elog.config.js
@@ -1,22 +1,27 @@
 module.exports = {
-  write: {
-    platform: 'notion',
-    notion: {
-      token: process.env.NOTION_TOKEN,
-      databaseId: process.env.NOTION_DATABASE_ID,
-      filter: { property: 'status', select: { equals: '已发布' }}
-    }
-  },
-  deploy: {
-    platform: 'halo',
-    halo: {
-      endpoint: process.env.HALO_ENDPOINT,
-      token: process.env.HALO_TOKEN,
-      policyName: process.env.HALO_POLICY_NAME,
-      needUploadImage: true
-    }
-  },
-  image: {
-    enable: false,
-  }
-}
+    write: {
+        platform: "notion",
+        notion: {
+            token: process.env.NOTION_TOKEN,
+            databaseId: process.env.NOTION_DATABASE_ID,
+            filter: {
+                and: [
+                    { property: "status", select: { equals: "Published" } },
+                    { property: "type", select: { equals: "Post" } },
+                ],
+            },
+        },
+    },
+    deploy: {
+        platform: "halo",
+        halo: {
+            endpoint: process.env.HALO_ENDPOINT,
+            token: process.env.HALO_TOKEN,
+            policyName: process.env.HALO_POLICY_NAME,
+            needUploadImage: true,
+        },
+    },
+    image: {
+        enable: false,
+    },
+};
--- elog.config.local.js
+++ elog.config.local.js
@@ -1,30 +1,35 @@
 module.exports = {
-  write: {
-    platform: 'notion',
-    notion: {
-      token: process.env.NOTION_TOKEN,
-      databaseId: process.env.NOTION_DATABASE_ID,
-      filter: { property: 'status', select: { equals: '已发布' }}
-    }
-  },
-  deploy: {
-    platform: 'local',
-    local: {
-      outputDir: './docs',
-      filename: 'title',
-      format: 'markdown',
-      frontMatter: {
+    write: {
+        platform: "notion",
+        notion: {
+            token: process.env.NOTION_TOKEN,
+            databaseId: process.env.NOTION_DATABASE_ID,
+            filter: {
+                and: [
+                    { property: "status", select: { equals: "Published" } },
+                    { property: "type", select: { equals: "Post" } },
+                ],
+            },
+        },
+    },
+    deploy: {
+        platform: "local",
+        local: {
+            outputDir: "./docs",
+            filename: "title",
+            format: "markdown",
+            frontMatter: {
+                enable: true,
+                exclude: ["cover"],
+            },
+        },
+    },
+    image: {
         enable: true,
-        exclude: ['cover']
-      }
-    }
-  },
-  image: {
-    enable: true,
-    platform: 'local',
-    local: {
-      outputDir: './images',
-      pathFollowDoc: true,
-    }
-  }
-}
+        platform: "local",
+        local: {
+            outputDir: "./images",
+            pathFollowDoc: true,
+        },
+    },
+};
